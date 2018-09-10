16c71fa70a26b70bfaff9fba331713b58ac0c3e1
yuwyuw <915998347@qq.com>
2018-09-10 14:26:10 +0800

    first add

---
+++ pages/index/index.js
@@ -0,0 +1,11 @@
+const app = getApp()
+Page({
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+})
+
